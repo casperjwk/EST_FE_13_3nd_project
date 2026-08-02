9586e4ba4dff5103df1945d8c345e1e22a5ad52c
isnow-x <cone2119@naver.com>
2026-08-02 20:54:51 +0900

    feat: 라우터 연결

---
+++ src/pages/auth/SignupPage.jsx
@@ -0,0 +1,9 @@
+function SignupPage() {
+  return (
+    <div>
+      <h2>회원가입 페이지</h2>
+    </div>
+  );
+}
+
+export default SignupPage;
---
+++ src/pages/my/FavoritePage.jsx
@@ -0,0 +1,9 @@
+function FavoritePage() {
+  return (
+    <div>
+      <h2>즐겨찾기 페이지</h2>
+    </div>
+  );
+}
+
+export default FavoritePage;
